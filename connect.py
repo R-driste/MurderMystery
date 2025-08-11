d9594618ffc490500076ed41c25e28104437fa8c
Dristi Roy <dristiroy@gmail.com>
2025-08-11 06:32:43 -0400

    hihihi

--- connect.py
+++ connect.py
@@ -5,6 +5,9 @@ class App(badge.BaseApp):
         self.all_active_contacts = []
         self.assignments = []
         self.host_id = 888
+        self.is_host = None
+        self.current_screen = "setup"
+        self.target = None
 
     def send_badge_id_to_host(self):
         my_contact = badge.contacts.my_contact()
@@ -18,13 +21,20 @@ class App(badge.BaseApp):
         self.logger.info(f"Successfully sent badge ID {badge_id} to host {self.host_id}.")
 
     def on_packet(self, packet: bytes, in_foreground: bool) -> None:
-        if len(packet) >= 4:
+        if self.is_host == True:
+            if len(packet) >= 4:
+                sender_badge_id = int.from_bytes(packet[0:4], "big")
+                contact = badge.contacts.get_contact_by_badge_id(sender_badge_id)
+                self.logger.info(f"Received badge ID and contact: {sender_badge_id}")
+                self.all_contacts.append(contact)
+            else:
+                self.logger.warning("Received malformed packet")
+        elif self.current_screen == "setup":
             sender_badge_id = int.from_bytes(packet[0:4], "big")
-            contact = badge.contacts.get_contact_by_badge_id(sender_badge_id)
-            self.logger.info(f"Received badge ID and contact: {sender_badge_id}")
-            self.all_contacts.append(contact)
-        else:
-            self.logger.warning("Received malformed packet")
+            self.target = badge.contacts.get_contact_by_badge_id(sender_badge_id)
+            self.logger.info(f"Received badge ID and contact of target: {sender_badge_id}")
+        elif self.current_screen == "hunt":
+            #something
 
     def assign_killers(self) -> None:
         if len(self.all_contacts) < 2:
@@ -33,6 +43,8 @@ class App(badge.BaseApp):
         else:
             needed_nums = self.get_pseudo_random()
             self.assignments = needed_nums
+            for contact, num in zip(self.all_available_contacts, needed_nums):
+
     
     def get_pseudo_random():
         valid = False
@@ -61,6 +73,16 @@ class App(badge.BaseApp):
             valid = True if len(fix) == num_len else False
         return fix
 
+    def render_screen(self) -> None:
+        badge.display.fill(1)
+        self.screens[self.current_screen]() #calls correct render
+        badge.display.show()
+
+    def render_home(self):
+        badge.display.nice_text("Welcome to\nKraken!", 0, 0, font=32, color=0)
+        badge.display.nice_text("Hang Tight", 0, 64, font=24, color=0)
+    
+    def
 """
 class Contact():
     def __init__(self, name: str):
